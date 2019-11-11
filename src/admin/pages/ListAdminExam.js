674c01de4973a19649f4c22978693bb2f0362513
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2019-11-12 00:06:39 +0700

    Update bank page

--- src/admin/pages/ListAdminExam.js
+++ src/admin/pages/ListAdminExam.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout/Layout';
 import './ListAdminExam.css';
+import ExamPage from './ExamPage';
+import axios from 'axios';
 
 class ListAdminExam extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { listExam: []};
+    }
+
+    async componentDidMount() {
+        axios
+        .get('http://localhost:5000/api/exam/list', {
+          headers: {
+            JWT:
+              'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ2MTc5NSwiZXhwIjoxNTc2MDUzNzk1fQ.IJ9D0PDlErcIhXQXXN3nv8SMGJMcnjmtbdkcAz79Pm9-ONwmb0eZQ-_-NEK1n8A3AfXcF1Ga03ZuPK7MwKl-J20jNIwpG-1cFr7fH4Lm5WMoXxd1RcJL_UGbcCEkZAYHZmMMwRs_s3pY_NA3hjvXlFLw1y2zca13cKJtKTnZLFNx5z382bdxzBw68Jk-ITi5lql8ufh67eOUatamKdZ4tVClb7lgr3-FtmZQc4z-omPW5B1VCXWIUzYFBddjEfqXtdMoVwEWeJUezAAv9X0vCu8Ae79rppQgMQgoSkfZP2VWtTURNe4xWjCtjNYOJvj3RfeGsc09egVZ3mP1wWql7A',
+          },
+        })
+        .then(response => {
+          console.log(response.data);
+          const data = response.data;
+          this.setState({ listExam: data });
+        });
+      }
+
+    render() { 
         return (
             <Layout>
                 <div class="content row">
@@ -23,102 +45,34 @@ class ListAdminExam extends Component {
                                 <div class="header-item-wrapper">
                                     <div class="fa fa-plus-square fomat-icon-menu"></div>
                                     <div class="item-text">Thêm đề thi</div>
-
                                 </div>
                             </div>
                         </div>
-
-                        <div class="white-box">
+                      
+                      {this.state.listExam.map((exam, index) =>{
+                          return (
+                            <div class="white-box" key={index} id={exam.id}>
                             <div class="bank-center">
                                 <div class="bank-body">
                                     <div class="bank-contnet">
                                         <div class="row justify-content-end header-wrapper-end">
-                                            <button type="button" class="btn btn-default btn-sm dt-delete" style={{ borderColor: "#fff" }} onclick="ConfirmDelete()">
+                                            <button type="button" class="btn btn-default btn-sm dt-delete" onClick="ConfirmDelete()">
                                                 <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
                                             </button>
                                         </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="time">Cập nhật lần cuối vào 03/09/2019</span>
+                                        <h4 class="exam-name">{exam.name}</h4>
+                                        <span class="Updatetime"><b>Sửa đổi lần cuối: </b>{exam.time}</span>
                                         <span class="bank-desc">
-                                            <span>Số lượng câu hỏi: 50</span>
-                                            <br />
-                                            <span>Dễ: 25</span>
-                                            <span>Trung bình: 15</span>
-                                            <span>Khó: 10</span>
+                                        <span><b>Mã đề thi: </b>{exam.password}</span>
                                         </span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                            <button type="button" class="btn btn-default btn-sm dt-delete" style={{ borderColor: "#fff" }} onclick="ConfirmDelete()">
-                                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                                            </button>
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="time">Cập nhật lần cuối vào 03/09/2019</span>
-                                        <span class="bank-desc">
-                                            <span>Số lượng câu hỏi: 50</span>
-                                            <br />
-                                            <span>Dễ: 25</span>
-                                            <span>Trung bình: 15</span>
-                                            <span>Khó: 10</span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                            <button type="button" class="btn btn-default btn-sm dt-delete" style={{ borderColor: "#fff" }} onclick="ConfirmDelete()">
-                                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                                            </button>
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="time">Cập nhật lần cuối vào 03/09/2019</span>
-                                        <span class="bank-desc">
-                                            <span>Số lượng câu hỏi: 50</span>
-                                            <br />
-                                            <span>Dễ: 25</span>
-                                            <span>Trung bình: 15</span>
-                                            <span>Khó: 10</span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                            <button type="button" class="btn btn-default btn-sm dt-delete" style={{ borderColor: "#fff" }} onclick="ConfirmDelete()">
-                                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                                            </button>
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="time">Cập nhật lần cuối vào 03/09/2019</span>
-                                        <span class="bank-desc">
-                                            <span>Số lượng câu hỏi: 50</span>
-                                            <br />
-                                            <span>Dễ: 25</span>
-                                            <span>Trung bình: 15</span>
-                                            <span>Khó: 10</span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    );
+                      })}
+                        
+                
                     </div>
                 </div>
             </Layout>
--- src/admin/pages/ListBankPage.js
+++ src/admin/pages/ListBankPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
+import Axios from 'axios';
 
 import Layout from '../components/Layout/Layout';
 import './ListBankPage.css';
@@ -9,70 +10,46 @@ import './ListBankPage.css';
 class ListBankPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      listBank: [
-        {
-          id: 'abc',
-          name: 'Công nghệ phần mềm',
-          description: 'Bộ câu hỏi ôn thi cuối kì',
-          modifiedDate: '03/12/1999',
-        },
-        {
-          id: 'XYZ',
-          name: 'ToaN',
-          description: 'Bộ câu hỏi ôn thi cuối kì',
-          modifiedDate: '03/12/1999',
-        },
-      ],
-    };
+    this.state = { listBank: [], currentEditBank:{} };
   }
 
   async componentDidMount() {
-    const cookies = new Cookies();
-    // await axios.post("http://localhost:5000/api/users/Login", {
-    //     "Username" : "Natalie7",
-    //     "Password" : "6         "
-    // }).then(res => res.data);
+    // const cookies = new Cookies();
+    // await cookies.set(
+    //   'JWT',
+    //   'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ2MTc5NSwiZXhwIjoxNTc2MDUzNzk1fQ.IJ9D0PDlErcIhXQXXN3nv8SMGJMcnjmtbdkcAz79Pm9-ONwmb0eZQ-_-NEK1n8A3AfXcF1Ga03ZuPK7MwKl-J20jNIwpG-1cFr7fH4Lm5WMoXxd1RcJL_UGbcCEkZAYHZmMMwRs_s3pY_NA3hjvXlFLw1y2zca13cKJtKTnZLFNx5z382bdxzBw68Jk-ITi5lql8ufh67eOUatamKdZ4tVClb7lgr3-FtmZQc4z-omPW5B1VCXWIUzYFBddjEfqXtdMoVwEWeJUezAAv9X0vCu8Ae79rppQgMQgoSkfZP2VWtTURNe4xWjCtjNYOJvj3RfeGsc09egVZ3mP1wWql7A',
+    //   { path: '/' },
+    // );
 
-    await cookies.set(
-      'JWT',
-      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ2MTc5NSwiZXhwIjoxNTc2MDUzNzk1fQ.IJ9D0PDlErcIhXQXXN3nv8SMGJMcnjmtbdkcAz79Pm9-ONwmb0eZQ-_-NEK1n8A3AfXcF1Ga03ZuPK7MwKl-J20jNIwpG-1cFr7fH4Lm5WMoXxd1RcJL_UGbcCEkZAYHZmMMwRs_s3pY_NA3hjvXlFLw1y2zca13cKJtKTnZLFNx5z382bdxzBw68Jk-ITi5lql8ufh67eOUatamKdZ4tVClb7lgr3-FtmZQc4z-omPW5B1VCXWIUzYFBddjEfqXtdMoVwEWeJUezAAv9X0vCu8Ae79rppQgMQgoSkfZP2VWtTURNe4xWjCtjNYOJvj3RfeGsc09egVZ3mP1wWql7A',
-      { path: '/' },
-    );
-    console.log(cookies.get('Cookie'));
-    // axios
-    //   .request(
-    //       {
-    //           url: 'http://192.168.43.33/api/bank/list',
-    //           method: "get",
-    //           headers: {
-    //             'Cookie': "JWT=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ1OTM0OCwiZXhwIjoxNTc2MDUxMzQ4fQ.dc-how-2zYCDzcfKHf353QiDiqUQiWaUM8WI6AsHB-nOopoE3stMG99fHX8cOLG0as9m143bya3iGmSQb5V8CLQf6P4AAmev5VJmN9cfLRpgQM5BObdIZUgKCz3_fMwiQt5WxachYNsvZwLXTt_6XRPPEnEUk67h-2GzGAXiL4FcIcnXTsfZlORMAthL5KONuxa6lwfFVbJ3b3wivyAPzT0uP42zYM41uK7_wjiW-Y8w3kvH-8Hc0piIkuwRovI1TxqBjXnbVyr7LhVQwNQ7gpWBRjUKDZYOwBZ4sfcXyh7_yvW3qxzNHrjL8PzjVYEHVlAVB-7e0eZqYdB7iPHG1w",
-    //             'content-Type': 'application/json',
-    //             'Accept': "/",
-    //             "Cache-Control": "no-cache"
-    //         }
-    //       })
-    //   .then(res => console.log(res.data));
+    // var url = 'http://localhost:5000/api/bank/list';
+    // var ck = 'JWT=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ2MTc5NSwiZXhwIjoxNTc2MDUzNzk1fQ.IJ9D0PDlErcIhXQXXN3nv8SMGJMcnjmtbdkcAz79Pm9-ONwmb0eZQ-_-NEK1n8A3AfXcF1Ga03ZuPK7MwKl-J20jNIwpG-1cFr7fH4Lm5WMoXxd1RcJL_UGbcCEkZAYHZmMMwRs_s3pY_NA3hjvXlFLw1y2zca13cKJtKTnZLFNx5z382bdxzBw68Jk-ITi5lql8ufh67eOUatamKdZ4tVClb7lgr3-FtmZQc4z-omPW5B1VCXWIUzYFBddjEfqXtdMoVwEWeJUezAAv9X0vCu8Ae79rppQgMQgoSkfZP2VWtTURNe4xWjCtjNYOJvj3RfeGsc09egVZ3mP1wWql7A';
 
-    const axiosConfig = {
-      headers: {
-        'content-Type': 'application/json',
-        Accept: '/',
-        'Cache-Control': 'no-cache',
-        Cookie: 'JWT=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ1OTM0OCwiZXhwIjoxNTc2MDUxMzQ4fQ.dc-how-2zYCDzcfKHf353QiDiqUQiWaUM8WI6AsHB-nOopoE3stMG99fHX8cOLG0as9m143bya3iGmSQb5V8CLQf6P4AAmev5VJmN9cfLRpgQM5BObdIZUgKCz3_fMwiQt5WxachYNsvZwLXTt_6XRPPEnEUk67h-2GzGAXiL4FcIcnXTsfZlORMAthL5KONuxa6lwfFVbJ3b3wivyAPzT0uP42zYM41uK7_wjiW-Y8w3kvH-8Hc0piIkuwRovI1TxqBjXnbVyr7LhVQwNQ7gpWBRjUKDZYOwBZ4sfcXyh7_yvW3qxzNHrjL8PzjVYEHVlAVB-7e0eZqYdB7iPHG1w' ,
-      },
-      credentials: 'same-origin',
-    };
-
-    axios.defaults.withCredentials = true;
     axios
-      .get('http://192.168.43.33/api/bank/list', axiosConfig)
-      .then(res => {
-        // Some result here
+      .get('http://localhost:5000/api/bank/list', {
+        headers: {
+          JWT:
+            'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IlVzZXJuYW1lIjoiTmF0YWxpZTciLCJQYXNzd29yZCI6IjYgICAgICAgICAiLCJJZCI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyJ9LCJ1bmlxdWVfbmFtZSI6ImUxZWM1NDQ0LTY1OTYtNWY1MC0xMzM4LTAwMTJiZjQ4OWZlNyIsImlzcyI6IiIsImlhdCI6MTU3MzQ2MTc5NSwiZXhwIjoxNTc2MDUzNzk1fQ.IJ9D0PDlErcIhXQXXN3nv8SMGJMcnjmtbdkcAz79Pm9-ONwmb0eZQ-_-NEK1n8A3AfXcF1Ga03ZuPK7MwKl-J20jNIwpG-1cFr7fH4Lm5WMoXxd1RcJL_UGbcCEkZAYHZmMMwRs_s3pY_NA3hjvXlFLw1y2zca13cKJtKTnZLFNx5z382bdxzBw68Jk-ITi5lql8ufh67eOUatamKdZ4tVClb7lgr3-FtmZQc4z-omPW5B1VCXWIUzYFBddjEfqXtdMoVwEWeJUezAAv9X0vCu8Ae79rppQgMQgoSkfZP2VWtTURNe4xWjCtjNYOJvj3RfeGsc09egVZ3mP1wWql7A',
+        },
       })
-      .catch(err => {
-        console.log(':(');
-      });
+      .then(response => {
+        console.log(response.data);
+        const data = response.data;
+        this.setState({ listBank: data });
+      });   
+  }
+
+  editBank(){
+      
+  }
+
+  onChangeBankName(event, bankId) {
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    const currentEdit = this.state.currentEditBank;
+    currentEdit.id = bankId;
+    currentEdit[name] = value;
+    this.setState({currentEditBank: currentEdit});
+    console.log(this.state.currentEditBank);
   }
 
   render() {
@@ -106,19 +83,20 @@ class ListBankPage extends Component {
                   <div class="bank-center p-t-10">
                     <div class="bank-body">
                       <div class="bank-contnet">
-                        <h4>{bank.name}</h4>
+                        <Link to={'/admin/bank/' + bank.id}>
+                          <h4>{bank.name}</h4>
+                        </Link>
                         <span class="pl-4">
                           Cập nhật lần cuối vào {bank.modifiedDate}
                         </span>
                         <span class="bank-desc">{bank.description}</span>
-                        <Link
-                          to="/admin/bank/abc"
+                        <button
                           class="btn btn btn-rounded btn-warning btn-outline  mr-1 text-white"
                           data-toggle="modal"
-                          data-target="#editBankModal"
+                          data-target={"#editBankModal" + bank.id}
                         >
                           <i class="ti-check"></i>Sửa
-                        </Link>
+                        </button>
                         <a
                           href="javacript:void(0)"
                           class="btn-rounded btn btn-danger btn-outline"
@@ -130,7 +108,7 @@ class ListBankPage extends Component {
                   </div>
                   <div
                     className="modal fade"
-                    id="editBankModal"
+                    id={"editBankModal" + bank.id}
                     tabIndex="-1"
                     role="dialog"
                     aria-labelledby="exampleModalCenterTitle"
@@ -138,7 +116,7 @@ class ListBankPage extends Component {
                   >
                     <div
                       className="modal-dialog modal-dialog-centered"
-                      role="document"
+                      role="document" 
                     >
                       <div className="modal-content">
                         <div className="modal-header">
@@ -165,14 +143,16 @@ class ListBankPage extends Component {
                             <input
                               class="form-control"
                               type="input"
-                              value={bank.name}
+                              name="name"
+                              defaultValue={bank.name}
+                              onChange={(event) => this.onChangeBankName(event, bank.id)}
                             ></input>
                           </div>
                           <div class="form-group">
                             <label for="exampleFormControlTextarea1">
                               Mô tả
                             </label>
-                            <textarea class="form-control">
+                            <textarea class="form-control" name="description" onChange={(event) => this.onChangeBankName(event, bank.id)}>
                               {bank.description}
                             </textarea>
                           </div>
@@ -182,6 +162,7 @@ class ListBankPage extends Component {
                             type="submit"
                             className="btn btn-secondary"
                             data-dismiss="modal"
+                            onClick={() => this.editBank()}
                           >
                             Lưu
                           </button>
