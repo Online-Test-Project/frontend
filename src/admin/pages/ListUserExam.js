06940c1b3484561c37008b2005620cd07188fe85
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2019-11-17 01:34:35 +0700

    Done UserExam and PreviewExam
    Xong 2 trang xem lại list đề đã thi và xem lại bài làm trong mỗi đề.

--- src/admin/pages/ListUserExam.js
+++ src/admin/pages/ListUserExam.js
@@ -1,83 +1,92 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout/Layout';
 import './ListUserExam.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import config from '../../_config/config';
+import { authHeader } from '../../_helpers/auth-header';
 
 class ListUserExam extends Component {
-    render() {
-        return (
-            <Layout>
-                <div class="content row">
-                    <div class="table-content">
-                        <div class="header-row-list">
-                            <div class="title">
-                                <h3 style={{ marginTop: "10px", marginLeft: "40px", fontWeight: "bold" }} >Đề thi đã làm</h3>
-                            </div>
-                            <div class="row justify-content-end header-wrapper-end">
-                                <div class="header-item-wrapper">
-                                    <div class="search-box">
-                                        <input placeholder="Tìm kiếm..." />
-                                            <span class="icon"><div class="fa fa-search fomat-icon-menu"></div></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="Updatetime">Thi vào ngày 03/09/2019</span>
-                                        <span class="bank-desc">Điểm: 9</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="Updatetime">Thi vào ngày 03/09/2019</span>
-                                        <span class="bank-desc">Điểm: 9</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="Updatetime">Thi vào ngày 03/09/2019</span>
-                                        <span class="bank-desc">Điểm: 9</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="white-box">
-                            <div class="bank-center">
-                                <div class="bank-body">
-                                    <div class="bank-contnet">
-                                        <div class="row justify-content-end header-wrapper-end">
-                                        </div>
-                                        <h4 class="exam-name">Công nghệ phần mềm (Giữa kì 19-20)</h4>
-                                        <span class="Updatetime">Thi vào ngày 03/09/2019</span>
-                                        <span class="bank-desc">Điểm: 9</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      listReviewExam: [],
+    };
+  }
+
+  async componentDidMount() {
+    axios
+      .get(config.SERVER_URL + '/api/review/list', {
+        headers: authHeader(),
+      })
+      .then(response => {
+        console.log(response.data);
+        const data = response.data;
+        this.setState({ listReviewExam: data });
+      });
+  }
+  render() {
+    return (
+      <Layout>
+        <div class="content row">
+          <div class="table-content">
+            <div class="header-row-list">
+              <div class="title">
+                <h3
+                  style={{
+                    marginTop: '10px',
+                    marginLeft: '40px',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  Đề thi đã làm
+                </h3>
+              </div>
+              <div class="row justify-content-end header-wrapper-end">
+                <div class="header-item-wrapper">
+                  <div class="search-box">
+                    <input placeholder="Tìm kiếm..." />
+                    <span class="icon">
+                      <div class="fa fa-search fomat-icon-menu"></div>
+                    </span>
+                  </div>
                 </div>
-            </Layout>
-        );
-    }
+              </div>
+            </div>
+
+            {this.state.listReviewExam.map((exam, index) => {
+              return (
+                <div class="white-box" key={index} id={exam.id}>
+                  <div class="bank-center">
+                    <div class="bank-body">
+                      <div class="bank-contnet">
+                        <div class="row justify-content-end header-wrapper-end"></div>
+                        <Link to={'/preview-exam/' + exam.id}>
+                          <h4>{exam.name}</h4>
+                        </Link>
+                        <span class="Updatetime">
+                          <b>Điểm: </b>
+                          {exam.score}
+                        </span>
+                        <span class="bank-desc">
+                          <b>Thời gian làm bài: </b>
+                          {exam.time}
+                        </span>
+                        <span class="bank-desc">
+                          <b>Ngày thi: </b>
+                          {exam.date}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default ListUserExam;
